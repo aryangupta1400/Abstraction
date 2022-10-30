eddb18de7cc24c94527f037faced80355abe7462
Aryan Gupta <Aryan.Gupta@oneadvanced.com>
2022-10-30 18:22:07 +0530

    modified

--- Abstraction/Program.cs
+++ Abstraction/Program.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿
+// 3) Print diffrent vehical details using Abstraction
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.AccessControl;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +14,74 @@ namespace Abstraction
     {
         static void Main(string[] args)
         {
+            Car car = new Car("Blue", 4, "Electric", 120);
+            car.display();
+
+            Truck truck = new Truck("Brown", 6);
+            truck.display();
+
+            Bike bike = new Bike("matt black", 2, "Royal Enfield Bullet");
+            bike.display();
+
+            Console.ReadKey();
         }
     }
+
+    abstract class Vehical
+    {
+        public string color;
+        public int tireCount;
+
+        public Vehical(string color, int tireCount)
+        {
+            this.color = color;
+            this.tireCount = tireCount;
+        }
+    }
+
+    class Car : Vehical
+    {
+        string carType;
+        int topSpeed;
+
+        public Car(string color, int tireCount, string carType, int topSpeed) : base(color, tireCount)
+        {
+            this.carType = carType;
+            this.topSpeed = topSpeed;
+        }
+
+        public void display()
+        {
+            Console.WriteLine("\nThat " + color + " car is " + carType + " with a top speed of " + topSpeed + "km/hr.");
+        }
+    }
+
+    class Truck : Vehical
+    {
+        public Truck(string color, int tireCount) : base(color, tireCount)
+        {
+
+        }
+
+        public void display()
+        {
+            Console.WriteLine("\nThat " + color + " truck has " + tireCount + " wheels.");
+        }
+    }
+
+    class Bike : Vehical
+    {
+        string model;
+
+        public Bike(string color, int tireCount, string model) : base(color, tireCount)
+        {
+            this.model = model;
+        }
+
+        public void display()
+        {
+            Console.WriteLine("\nThat " + model + " looks sporty in " + color + " color.");
+        }
+    }
+
 }
